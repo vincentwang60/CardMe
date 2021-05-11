11b6b916027547b2910666a89c377ebc5ba7eb4c
Vincent Wang <vincent.wang.60@gmail.com>
2021-05-11 15:08:04 -0700

    added screens and navigator

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Inter_300Light,
+  Inter_600SemiBold,
+} from '@expo-google-fonts/inter';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import signUpScreen from './src/components/signUpScreen';
+import homeScreen from './src/components/homeScreen';
+import informationScreen from './src/components/informationScreen';
+import libraryScreen from './src/components/libraryScreen';
+import styleSelectScreen from './src/components/styleSelectScreen';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Text!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      mode="modal"
+      screenOptions={{
+        animationEnabled: false,
+        headerStyle: {
+          backgroundColor: '#040F0F',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontFamily: 'Inter_600SemiBold',
+        },
+      }}
+    >
+      <Stack.Screen name="signUpScreen" component={signUpScreen} />
+      <Stack.Screen name="homeScreen" component={homeScreen} />
+      <Stack.Screen name="informationScreen" component={informationScreen} />
+      <Stack.Screen name="libraryScreen" component={libraryScreen} />
+      <Stack.Screen name="styleSelectScreen" component={styleSelectScreen} />
+    </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_300Light,
+    Inter_600SemiBold,
+  });
+  if (!fontsLoaded){
+    return <AppLoading />;
+  }
+  else{
+    return (
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    );
+  }
+}
---
+++ src/components/signUpScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, Button, View, StatusBar, TouchableOpacity } from 'react-native';
+
+export default function homeScreen( {navigation }) {
+  const pressHandler = () => {
+    navigation.navigate('informationScreen')
+  }
+  return (
+    <View style={styles.container}>
+      <Text style = {[styles.welcomeText]}>Quick brown fox jumps over{"\n"}the lazy dog</Text>
+      <TouchableOpacity onPress={pressHandler}>
+         <Text style = {[styles.buttonText]}>
+             Button
+         </Text>
+      </TouchableOpacity >
+      <StatusBar
+        barStyle = "light-content"
+        backgroundColor = '#000'/>
+    </View>
+  );
+}
+
+//https://reactnative.dev/docs/style
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#325F71',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeText: {
+    textAlign: 'center',
+    fontSize: 24,
+    color: '#fff',
+    fontFamily: 'Inter_300Light',
+    flex:0.15
+  },
+  buttonText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderColor: 'black',
+    backgroundColor: '#AACCDA',
+    fontSize: 20,
+    color: 'black',
+    fontFamily: 'Inter_600SemiBold',
+  }
+});
