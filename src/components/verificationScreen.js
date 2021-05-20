2d3146c1db72aeebcf9812831851c291df28fc4b
Vincent Wang <vincent.wang.60@gmail.com>
2021-05-20 16:43:16 -0700

    numeric keyboard verification screen

--- src/components/verificationScreen.js
+++ src/components/verificationScreen.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useRef} from 'react';
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity, TextInput } from 'react-native';
+import React, {useState, useRef} from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, TextInput, Pressable } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 
 import Amplify, {Auth} from "aws-amplify";
@@ -11,27 +11,40 @@ import Button from './shared/button.js';
 export default function verificationScreen( {route, navigation }) {
   const {passedEmail} = route.params;
   const {passedPassword} = route.params;
-
-  const [input1, setInput1] = useState('');
-  const [input2, setInput2] = useState('');
-  const [input3, setInput3] = useState('');
-  const [input4, setInput4] = useState('');
-  const [input5, setInput5] = useState('');
-  const [input6, setInput6] = useState('');
-  const ref_input1 = useRef();
-  const ref_input2 = useRef();
-  const ref_input3 = useRef();
-  const ref_input4 = useRef();
-  const ref_input5 = useRef();
-  const ref_input6 = useRef();
   const [confirmationCode, setConfirmationCode] = useState('');
+  const [containerIsFocused, setContainerIsFocused] = useState(false);
+  const codeDigitsArray = new Array(6).fill(0)
+  const ref = useRef();
 
+  function handleOnPress() {
+    setContainerIsFocused(true);
+    console.log('pressed', containerIsFocused)
+    ref.current.focus();
+  }
+  const toDigitInput = (_value: number, i: number) =>{
+    const emptyInputchar = '  '
+    const digit = confirmationCode[i] || emptyInputchar
+
+    const isCurrentDigit = i === confirmationCode.length;
+    const isLastDigit = i === 5;
+    const isCodeFull = confirmationCode.length === 6;
+    const isFocused = isCurrentDigit || (isLastDigit && isCodeFull);
+
+    const inputStyle = containerIsFocused && isFocused
+        ? {...styles.inputContainer, ...styles.inputContainerFocused}
+        : styles.inputContainer;
+
+    return(
+      <View style={inputStyle} key={i}>
+        <Text style = {styles.inputText}>{digit}</Text>
+      </View>
+    );
+  }
   function toggleIsNewUser() {
     setIsNewUser(!isNewUser);
   }
   function confirmSignUp(){
-    const code = input1+input2+input3+input4+input5+input6
-    Auth.confirmSignUp(passedEmail, code)
+    Auth.confirmSignUp(passedEmail, confirmationCode)
     .then(()=>{
       console.log('confirm success', passedEmail);
       signIn()
@@ -54,84 +67,20 @@ export default function verificationScreen( {route, navigation }) {
     <LinearGradient colors={['#fff','#F4F4F4']} style={styles.container}>
       <Text style={[styles.text, { top: '10.9%'}]}>Authenticate your account!</Text>
       <Text style={[styles.text, { top: '15.2%'}, {color: '#8F8F8F'}]}>We sent a verification code to{'\n'}your email address. </Text>
-      <Text style={styles.labelStyle}> Verfication code </Text>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput}
-          autoFocus = {true}
-          keyboardType = {'numeric'}
-          autoCapitalize = {"characters"}
-          onChangeText={(text) => setInput1(text)}
-          onKeyPress={({ nativeEvent }) => {
-            if(nativeEvent.key != 'Backspace') {ref_input2.current.focus()}
-          }}
-          maxLength={1}
-          ref={ref_input1}
-        />
-        <TextInput style={styles.textInput}
-          autoCapitalize = {"characters"}
-          onChangeText={(text) => setInput2(text)}
-          onKeyPress={({ nativeEvent }) => {
-            if(nativeEvent.key === 'Backspace'){
-              ref_input1.current.focus();
-              if (input2 == ''){ ref_input1.current.clear(); setInput1('') }
-            }
-            else{ref_input3.current.focus()}
-          }}
-          maxLength={1}
-          ref={ref_input2}
-        />
-        <TextInput style={styles.textInput}
-          autoCapitalize = {"characters"}
-          onChangeText={(text) => setInput3(text)}
-          onKeyPress={({ nativeEvent }) => {
-            if(nativeEvent.key === 'Backspace'){
-              ref_input2.current.focus();
-              if (input3 == ''){ ref_input2.current.clear(); setInput2('') }
-            }
-            else{ref_input4.current.focus()}
-          }}
-          maxLength={1}
-          ref={ref_input3}
-        />
-        <TextInput style={styles.textInput}
-          autoCapitalize = {"characters"}
-          onChangeText={(text) => setInput4(text)}
-          onKeyPress={({ nativeEvent }) => {
-            if(nativeEvent.key === 'Backspace'){
-              ref_input3.current.focus();
-              if (input4 == ''){ ref_input3.current.clear(); setInput3('') }
-            }
-            else{ref_input5.current.focus()}
-          }}
-          maxLength={1}
-          ref={ref_input4}
-        />
-        <TextInput style={styles.textInput}
-          autoCapitalize = {"characters"}
-          onChangeText={(text) => setInput5(text)}
-          onKeyPress={({ nativeEvent }) => {
-            if(nativeEvent.key === 'Backspace'){
-              ref_input4.current.focus();
-              if (input5 == ''){ ref_input4.current.clear(); setInput4('') }
-            }
-            else{ref_input6.current.focus()}
-          }}
-          maxLength={1}
-          ref={ref_input5}
-        />
-        <TextInput style={styles.textInput}
-          autoCapitalize = {"characters"}
-          onChangeText={(text) => setInput6(text)}
-          onKeyPress={({nativeEvent}) =>{
-            if(nativeEvent.key === 'Backspace'){
-              ref_input5.current.focus();
-              if (input6 == ''){ ref_input5.current.clear(); setInput5('') }
-            }
-          }}
-          maxLength={1}
-          ref={ref_input6}
-        />
-      </View>
+      <Text style={styles.labelStyle}> Verification code </Text>
+      <Pressable style={styles.inputsContainer} onPress={handleOnPress}>
+        {codeDigitsArray.map(toDigitInput)}
+      </Pressable>
+      <TextInput
+        ref = {ref}
+        value={confirmationCode}
+        onChangeText={setConfirmationCode}
+        keyboardType="number-pad"
+        returnKeyDone="done"
+        textContentType="oneTimeCode"
+        maxLength={6}
+        style={styles.hiddenCodeInput}
+      />
       <Button containerStyle={[styles.textContainer, { top: '81.5%'}]} label="Authenticate account" onPress={() => confirmSignUp()} />
       <View style={styles.textContainer}>
         <Text style={[styles.signInText]}>Haven't received it? </Text>
@@ -148,21 +97,40 @@ export default function verificationScreen( {route, navigation }) {
 
 //https://reactnative.dev/docs/style
 const styles = StyleSheet.create({
+  inputsContainer:{
+    position: 'absolute',
+    justifyContent: 'space-evenly',
+    width: "100%",
+    top: "34%",
+    flexDirection: 'row',
+  },
+  inputContainer:{
+    width: "10%",
+    alignItems: 'center',
+    borderBottomColor:'#8F8F8F',
+    borderBottomWidth: 1,
+  },
+  inputContainerFocused:{
+    borderBottomColor:'#000',
+  },
+  inputText:{
+    fontSize: 30,
+    fontFamily: 'Nunito_700Bold',
+  },
+  hiddenCodeInput:{
+    position:'absolute',
+    height: 0,
+    width:0,
+    opacity: 0,
+    backgroundColor: '#f0f'
+  },
   labelStyle:{
     position: 'absolute',
     left: "6.2%",
-    top: "28%",
+    top: "29%",
     color: '#8F8F8F',
     fontSize:15,
     fontFamily: 'Nunito_700Bold',
-  },
-  inputContainer:{
-    position: 'absolute',
-    left: "6.2%",
-    width: "85.3%",
-    top: "33.5%",
-    justifyContent: 'space-around',
-    flexDirection: 'row',
   },
   textContainer:{
     position: 'absolute',
@@ -175,14 +143,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     justifyContent: 'center',
     flex: 1,
-  },
-  textInput:{
-    fontSize: 30,
-    textAlign: 'center',
-    width: 30,
-    borderBottomColor: '#8F8F8F',
-    borderBottomWidth: 1,
-    fontFamily: 'Nunito_700Bold',
   },
   text:{
     fontSize: 20,
